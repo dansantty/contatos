e0e3d405447408a8794940ec797cb244db4a587e
dansantty <34076157+dansantty@users.noreply.github.com>
2018-02-14 17:58:42 -0200

    Add files via upload

---
+++ Contatos/Contatos/DADOS.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contatos;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Contatos
+{
+    public class DADOS
+    {
+        private int _idPessoa;
+        private string _nome;
+        private DateTime _nasc;
+        private string celular;
+        private string residencia;
+        private string comercial;
+        private string fax;
+        private string pessoal;
+        private string profissional;
+
+        public int IdPessoa
+        {
+            get
+            {
+                return _idPessoa;
+            }
+
+            set
+            {
+                _idPessoa = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public DateTime Nasc
+        {
+            get
+            {
+                return _nasc;
+            }
+
+            set
+            {
+                _nasc = value;
+            }
+        }
+
+        public string Celular
+        {
+            get
+            {
+                return celular;
+            }
+
+            set
+            {
+                celular = value;
+            }
+        }
+
+        public string Residencia
+        {
+            get
+            {
+                return residencia;
+            }
+
+            set
+            {
+                residencia = value;
+            }
+        }
+
+        public string Comercial
+        {
+            get
+            {
+                return comercial;
+            }
+
+            set
+            {
+                comercial = value;
+            }
+        }
+
+        public string Fax
+        {
+            get
+            {
+                return fax;
+            }
+
+            set
+            {
+                fax = value;
+            }
+        }
+
+        public string Pessoal
+        {
+            get
+            {
+                return pessoal;
+            }
+
+            set
+            {
+                pessoal = value;
+            }
+        }
+
+        public string Profissional
+        {
+            get
+            {
+                return profissional;
+            }
+
+            set
+            {
+                profissional = value;
+            }
+        }
+
+        public DADOS()
+        {
+
+        }
+
+        public DADOS(int idPessoa, string nome, DateTime nasc, string celular, string residencial, string comercial,
+            string fax, string pessoal, string profissional)
+        {
+            this.IdPessoa = idPessoa;
+            this.Nome = nome;
+            this.Nasc = nasc;
+            this.Celular = celular;
+            this.Residencia = residencial;
+            this.Comercial = comercial;
+            this.Fax = fax;
+            this.Pessoal = pessoal;
+            this.Profissional = profissional;
+          
+        }
+
+        public string InserirMorador(DADOS Contato)
+        {
+            string resp = "";
+            SqlConnection sqlCon = new SqlConnection();
+
+            try
+            {
+                //codigo
+                sqlCon.ConnectionString = Conexao.Cn;
+                sqlCon.Open();
+
+                SqlCommand sqlCmd = new SqlCommand();
+                sqlCmd.Connection = sqlCon;
+                sqlCmd.CommandText = "inserirContato";
+                sqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter parIdPessoa = new SqlParameter();
+                parIdPessoa.ParameterName = "@idPessoa";
+                parIdPessoa.SqlDbType = SqlDbType.Int;
+                parIdPessoa.Direction = ParameterDirection.InputOutput;
+                sqlCmd.Parameters.Add(parIdPessoa);
+
+                SqlParameter parNome = new SqlParameter();
+                parNome.ParameterName = "@nome";
+                parNome.SqlDbType = SqlDbType.VarChar;
+                parNome.Size = 25;
+                parNome.Value = Contato.Nome;
+                sqlCmd.Parameters.Add(parNome);
+
+                SqlParameter parNasc = new SqlParameter();
+                parNasc.ParameterName = "@nasc";
+                parNasc.SqlDbType = SqlDbType.Date;
+                parNasc.Value = Contato.Nasc;
+                sqlCmd.Parameters.Add(parNasc);
+
+                SqlParameter parNome = new SqlParameter();
+                parNome.ParameterName = "@nome";
+                parNome.SqlDbType = SqlDbType.VarChar;
+                parNome.Size = 25;
+                parNome.Value = Contato.Nome;
+                sqlCmd.Parameters.Add(parNome);
+
+                SqlParameter parNome = new SqlParameter();
+                parNome.ParameterName = "@nome";
+                parNome.SqlDbType = SqlDbType.VarChar;
+                parNome.Size = 25;
+                parNome.Value = Contato.Nome;
+                sqlCmd.Parameters.Add(parNome);
+
+                SqlParameter parNome = new SqlParameter();
+                parNome.ParameterName = "@nome";
+                parNome.SqlDbType = SqlDbType.VarChar;
+                parNome.Size = 25;
+                parNome.Value = Contato.Nome;
+                sqlCmd.Parameters.Add(parNome);
+
+                SqlParameter parNome = new SqlParameter();
+                parNome.ParameterName = "@nome";
+                parNome.SqlDbType = SqlDbType.VarChar;
+                parNome.Size = 25;
+                parNome.Value = Contato.Nome;
+                sqlCmd.Parameters.Add(parNome);
+
+
+                //executar comando
+
+                resp = sqlCmd.ExecuteNonQuery() == 1 ? "Contato Inserido" : "Xonttato não inserido";
+
+
+
+            }
+            catch (Exception ex)
+            {
+                resp = ex.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                    sqlCon.Close();
+            }
+            return resp;
+        }
+
+    }
+}
---
+++ Contatos/Contatos/Form1.Designer.cs
@@ -0,0 +1,436 @@
+﻿namespace Contatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.llA = new System.Windows.Forms.LinkLabel();
+            this.llB = new System.Windows.Forms.LinkLabel();
+            this.llE = new System.Windows.Forms.LinkLabel();
+            this.llC = new System.Windows.Forms.LinkLabel();
+            this.llD = new System.Windows.Forms.LinkLabel();
+            this.llF = new System.Windows.Forms.LinkLabel();
+            this.llG = new System.Windows.Forms.LinkLabel();
+            this.llH = new System.Windows.Forms.LinkLabel();
+            this.llI = new System.Windows.Forms.LinkLabel();
+            this.llK = new System.Windows.Forms.LinkLabel();
+            this.llJ = new System.Windows.Forms.LinkLabel();
+            this.llL = new System.Windows.Forms.LinkLabel();
+            this.llM = new System.Windows.Forms.LinkLabel();
+            this.llN = new System.Windows.Forms.LinkLabel();
+            this.llO = new System.Windows.Forms.LinkLabel();
+            this.llP = new System.Windows.Forms.LinkLabel();
+            this.llQ = new System.Windows.Forms.LinkLabel();
+            this.llR = new System.Windows.Forms.LinkLabel();
+            this.llS = new System.Windows.Forms.LinkLabel();
+            this.llU = new System.Windows.Forms.LinkLabel();
+            this.llT = new System.Windows.Forms.LinkLabel();
+            this.llV = new System.Windows.Forms.LinkLabel();
+            this.llX = new System.Windows.Forms.LinkLabel();
+            this.llZ = new System.Windows.Forms.LinkLabel();
+            this.llW = new System.Windows.Forms.LinkLabel();
+            this.llY = new System.Windows.Forms.LinkLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.llTodos = new System.Windows.Forms.LinkLabel();
+            this.contatoDataSet = new Contatos.contatoDataSet();
+            this.contatoDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contatoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contatoDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // llA
+            // 
+            this.llA.AutoSize = true;
+            this.llA.Location = new System.Drawing.Point(12, 9);
+            this.llA.Name = "llA";
+            this.llA.Size = new System.Drawing.Size(14, 13);
+            this.llA.TabIndex = 0;
+            this.llA.TabStop = true;
+            this.llA.Text = "A";
+            // 
+            // llB
+            // 
+            this.llB.AutoSize = true;
+            this.llB.Location = new System.Drawing.Point(32, 9);
+            this.llB.Name = "llB";
+            this.llB.Size = new System.Drawing.Size(14, 13);
+            this.llB.TabIndex = 1;
+            this.llB.TabStop = true;
+            this.llB.Text = "B";
+            // 
+            // llE
+            // 
+            this.llE.AutoSize = true;
+            this.llE.Location = new System.Drawing.Point(92, 9);
+            this.llE.Name = "llE";
+            this.llE.Size = new System.Drawing.Size(14, 13);
+            this.llE.TabIndex = 2;
+            this.llE.TabStop = true;
+            this.llE.Text = "E";
+            // 
+            // llC
+            // 
+            this.llC.AutoSize = true;
+            this.llC.Location = new System.Drawing.Point(52, 9);
+            this.llC.Name = "llC";
+            this.llC.Size = new System.Drawing.Size(14, 13);
+            this.llC.TabIndex = 3;
+            this.llC.TabStop = true;
+            this.llC.Text = "C";
+            // 
+            // llD
+            // 
+            this.llD.AutoSize = true;
+            this.llD.Location = new System.Drawing.Point(72, 9);
+            this.llD.Name = "llD";
+            this.llD.Size = new System.Drawing.Size(15, 13);
+            this.llD.TabIndex = 4;
+            this.llD.TabStop = true;
+            this.llD.Text = "D";
+            // 
+            // llF
+            // 
+            this.llF.AutoSize = true;
+            this.llF.Location = new System.Drawing.Point(112, 9);
+            this.llF.Name = "llF";
+            this.llF.Size = new System.Drawing.Size(13, 13);
+            this.llF.TabIndex = 5;
+            this.llF.TabStop = true;
+            this.llF.Text = "F";
+            // 
+            // llG
+            // 
+            this.llG.AutoSize = true;
+            this.llG.Location = new System.Drawing.Point(132, 9);
+            this.llG.Name = "llG";
+            this.llG.Size = new System.Drawing.Size(15, 13);
+            this.llG.TabIndex = 6;
+            this.llG.TabStop = true;
+            this.llG.Text = "G";
+            // 
+            // llH
+            // 
+            this.llH.AutoSize = true;
+            this.llH.Location = new System.Drawing.Point(152, 9);
+            this.llH.Name = "llH";
+            this.llH.Size = new System.Drawing.Size(15, 13);
+            this.llH.TabIndex = 7;
+            this.llH.TabStop = true;
+            this.llH.Text = "H";
+            // 
+            // llI
+            // 
+            this.llI.AutoSize = true;
+            this.llI.Location = new System.Drawing.Point(172, 9);
+            this.llI.Name = "llI";
+            this.llI.Size = new System.Drawing.Size(10, 13);
+            this.llI.TabIndex = 8;
+            this.llI.TabStop = true;
+            this.llI.Text = "I";
+            // 
+            // llK
+            // 
+            this.llK.AutoSize = true;
+            this.llK.Location = new System.Drawing.Point(212, 9);
+            this.llK.Name = "llK";
+            this.llK.Size = new System.Drawing.Size(14, 13);
+            this.llK.TabIndex = 9;
+            this.llK.TabStop = true;
+            this.llK.Text = "K";
+            // 
+            // llJ
+            // 
+            this.llJ.AutoSize = true;
+            this.llJ.Location = new System.Drawing.Point(192, 9);
+            this.llJ.Name = "llJ";
+            this.llJ.Size = new System.Drawing.Size(12, 13);
+            this.llJ.TabIndex = 10;
+            this.llJ.TabStop = true;
+            this.llJ.Text = "J";
+            // 
+            // llL
+            // 
+            this.llL.AutoSize = true;
+            this.llL.Location = new System.Drawing.Point(232, 9);
+            this.llL.Name = "llL";
+            this.llL.Size = new System.Drawing.Size(13, 13);
+            this.llL.TabIndex = 11;
+            this.llL.TabStop = true;
+            this.llL.Text = "L";
+            // 
+            // llM
+            // 
+            this.llM.AutoSize = true;
+            this.llM.Location = new System.Drawing.Point(252, 9);
+            this.llM.Name = "llM";
+            this.llM.Size = new System.Drawing.Size(16, 13);
+            this.llM.TabIndex = 12;
+            this.llM.TabStop = true;
+            this.llM.Text = "M";
+            // 
+            // llN
+            // 
+            this.llN.AutoSize = true;
+            this.llN.Location = new System.Drawing.Point(272, 9);
+            this.llN.Name = "llN";
+            this.llN.Size = new System.Drawing.Size(15, 13);
+            this.llN.TabIndex = 13;
+            this.llN.TabStop = true;
+            this.llN.Text = "N";
+            // 
+            // llO
+            // 
+            this.llO.AutoSize = true;
+            this.llO.Location = new System.Drawing.Point(292, 9);
+            this.llO.Name = "llO";
+            this.llO.Size = new System.Drawing.Size(15, 13);
+            this.llO.TabIndex = 14;
+            this.llO.TabStop = true;
+            this.llO.Text = "O";
+            // 
+            // llP
+            // 
+            this.llP.AutoSize = true;
+            this.llP.Location = new System.Drawing.Point(312, 9);
+            this.llP.Name = "llP";
+            this.llP.Size = new System.Drawing.Size(14, 13);
+            this.llP.TabIndex = 15;
+            this.llP.TabStop = true;
+            this.llP.Text = "P";
+            // 
+            // llQ
+            // 
+            this.llQ.AutoSize = true;
+            this.llQ.Location = new System.Drawing.Point(332, 9);
+            this.llQ.Name = "llQ";
+            this.llQ.Size = new System.Drawing.Size(15, 13);
+            this.llQ.TabIndex = 16;
+            this.llQ.TabStop = true;
+            this.llQ.Text = "Q";
+            // 
+            // llR
+            // 
+            this.llR.AutoSize = true;
+            this.llR.Location = new System.Drawing.Point(352, 9);
+            this.llR.Name = "llR";
+            this.llR.Size = new System.Drawing.Size(15, 13);
+            this.llR.TabIndex = 17;
+            this.llR.TabStop = true;
+            this.llR.Text = "R";
+            // 
+            // llS
+            // 
+            this.llS.AutoSize = true;
+            this.llS.Location = new System.Drawing.Point(372, 9);
+            this.llS.Name = "llS";
+            this.llS.Size = new System.Drawing.Size(14, 13);
+            this.llS.TabIndex = 18;
+            this.llS.TabStop = true;
+            this.llS.Text = "S";
+            // 
+            // llU
+            // 
+            this.llU.AutoSize = true;
+            this.llU.Location = new System.Drawing.Point(412, 9);
+            this.llU.Name = "llU";
+            this.llU.Size = new System.Drawing.Size(15, 13);
+            this.llU.TabIndex = 19;
+            this.llU.TabStop = true;
+            this.llU.Text = "U";
+            // 
+            // llT
+            // 
+            this.llT.AutoSize = true;
+            this.llT.Location = new System.Drawing.Point(392, 9);
+            this.llT.Name = "llT";
+            this.llT.Size = new System.Drawing.Size(14, 13);
+            this.llT.TabIndex = 20;
+            this.llT.TabStop = true;
+            this.llT.Text = "T";
+            // 
+            // llV
+            // 
+            this.llV.AutoSize = true;
+            this.llV.Location = new System.Drawing.Point(432, 9);
+            this.llV.Name = "llV";
+            this.llV.Size = new System.Drawing.Size(14, 13);
+            this.llV.TabIndex = 21;
+            this.llV.TabStop = true;
+            this.llV.Text = "V";
+            // 
+            // llX
+            // 
+            this.llX.AutoSize = true;
+            this.llX.Location = new System.Drawing.Point(452, 9);
+            this.llX.Name = "llX";
+            this.llX.Size = new System.Drawing.Size(14, 13);
+            this.llX.TabIndex = 22;
+            this.llX.TabStop = true;
+            this.llX.Text = "X";
+            // 
+            // llZ
+            // 
+            this.llZ.AutoSize = true;
+            this.llZ.Location = new System.Drawing.Point(472, 9);
+            this.llZ.Name = "llZ";
+            this.llZ.Size = new System.Drawing.Size(14, 13);
+            this.llZ.TabIndex = 23;
+            this.llZ.TabStop = true;
+            this.llZ.Text = "Z";
+            this.llZ.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llZ_LinkClicked);
+            // 
+            // llW
+            // 
+            this.llW.AutoSize = true;
+            this.llW.Location = new System.Drawing.Point(510, 9);
+            this.llW.Name = "llW";
+            this.llW.Size = new System.Drawing.Size(18, 13);
+            this.llW.TabIndex = 24;
+            this.llW.TabStop = true;
+            this.llW.Text = "W";
+            // 
+            // llY
+            // 
+            this.llY.AutoSize = true;
+            this.llY.Location = new System.Drawing.Point(490, 9);
+            this.llY.Name = "llY";
+            this.llY.Size = new System.Drawing.Size(14, 13);
+            this.llY.TabIndex = 25;
+            this.llY.TabStop = true;
+            this.llY.Text = "Y";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.contatoDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 33);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(971, 376);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // llTodos
+            // 
+            this.llTodos.AutoSize = true;
+            this.llTodos.Location = new System.Drawing.Point(579, 9);
+            this.llTodos.Name = "llTodos";
+            this.llTodos.Size = new System.Drawing.Size(37, 13);
+            this.llTodos.TabIndex = 27;
+            this.llTodos.TabStop = true;
+            this.llTodos.Text = "Todos";
+            this.llTodos.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llTodos_LinkClicked);
+            // 
+            // contatoDataSet
+            // 
+            this.contatoDataSet.DataSetName = "contatoDataSet";
+            this.contatoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // contatoDataSetBindingSource
+            // 
+            this.contatoDataSetBindingSource.DataSource = this.contatoDataSet;
+            this.contatoDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 421);
+            this.Controls.Add(this.llTodos);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.llY);
+            this.Controls.Add(this.llW);
+            this.Controls.Add(this.llZ);
+            this.Controls.Add(this.llX);
+            this.Controls.Add(this.llV);
+            this.Controls.Add(this.llT);
+            this.Controls.Add(this.llU);
+            this.Controls.Add(this.llS);
+            this.Controls.Add(this.llR);
+            this.Controls.Add(this.llQ);
+            this.Controls.Add(this.llP);
+            this.Controls.Add(this.llO);
+            this.Controls.Add(this.llN);
+            this.Controls.Add(this.llM);
+            this.Controls.Add(this.llL);
+            this.Controls.Add(this.llJ);
+            this.Controls.Add(this.llK);
+            this.Controls.Add(this.llI);
+            this.Controls.Add(this.llH);
+            this.Controls.Add(this.llG);
+            this.Controls.Add(this.llF);
+            this.Controls.Add(this.llD);
+            this.Controls.Add(this.llC);
+            this.Controls.Add(this.llE);
+            this.Controls.Add(this.llB);
+            this.Controls.Add(this.llA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contatoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.contatoDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel llA;
+        private System.Windows.Forms.LinkLabel llB;
+        private System.Windows.Forms.LinkLabel llE;
+        private System.Windows.Forms.LinkLabel llC;
+        private System.Windows.Forms.LinkLabel llD;
+        private System.Windows.Forms.LinkLabel llF;
+        private System.Windows.Forms.LinkLabel llG;
+        private System.Windows.Forms.LinkLabel llH;
+        private System.Windows.Forms.LinkLabel llI;
+        private System.Windows.Forms.LinkLabel llK;
+        private System.Windows.Forms.LinkLabel llJ;
+        private System.Windows.Forms.LinkLabel llL;
+        private System.Windows.Forms.LinkLabel llM;
+        private System.Windows.Forms.LinkLabel llN;
+        private System.Windows.Forms.LinkLabel llO;
+        private System.Windows.Forms.LinkLabel llP;
+        private System.Windows.Forms.LinkLabel llQ;
+        private System.Windows.Forms.LinkLabel llR;
+        private System.Windows.Forms.LinkLabel llS;
+        private System.Windows.Forms.LinkLabel llU;
+        private System.Windows.Forms.LinkLabel llT;
+        private System.Windows.Forms.LinkLabel llV;
+        private System.Windows.Forms.LinkLabel llX;
+        private System.Windows.Forms.LinkLabel llZ;
+        private System.Windows.Forms.LinkLabel llW;
+        private System.Windows.Forms.LinkLabel llY;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.LinkLabel llTodos;
+        private System.Windows.Forms.BindingSource contatoDataSetBindingSource;
+        private contatoDataSet contatoDataSet;
+    }
+}
+
